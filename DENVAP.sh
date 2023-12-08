3774fa0a2194e6907724d9a58badd18541d5aafc
inrb-labgenpath <labgenpath@inrb.cd>
2023-12-08 17:54:28 +0100

    updates

--- DENVAP.sh
+++ DENVAP.sh
@@ -1,82 +1,77 @@
 #! usr/bin/bash
 
-mkdir {bams,consensus,dengue_reads,human_reads,index,mapping,non_dengue_reads,non_human_reads,reads,trimmed_reads,visualization_af,visualization_bf,results_dengue} &&
+mkdir {bams,consensus,dengue_reads,human_reads,index,mapping,non_dengue_reads,non_human_reads,reads,trimmed_reads,visualization_af,visualization_bf,results_dengue} ;
 
 cd fastq;
-for file in *barcode*; do cat $file/*.fastq > `echo $file`.fastq | chmod +x `echo $file`.fastq;done && 
+for file in *barcode*; do cat $file/*.fastq > `echo $file`.fastq | chmod +x `echo $file`.fastq;done ; 
 mv *.fastq ../reads ; cd .. 
 
 cd fastq;
-for i in *barcode??; do artic guppyplex --directory $i --min-length 150 --max-length 1500 --prefix ../trimmed_reads/DENV --quality 10;done &&  
+for i in *barcode??; do artic guppyplex --directory $i --min-length 150 --max-length 1500 --prefix ../trimmed_reads/DENV --quality 10;done ;  
 cd .. 
 
 snakemake -j1 -c10 -s visualization_bf.smk;
 snakemake -j1 -c10 -s visualization_af.smk;
 
 cd trimmed_reads;
-for i in *.fastq; do minimap2 -a -x map-ont -t 10 ../index/GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna.gz `echo ${i:0:29}`.fastq -o ../mapping/`echo ${i:0:29}`.bam; done && 
+for i in *.fastq; do minimap2 -a -x map-ont -t 10 ../index/GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna.gz `echo ${i:0:29}`.fastq -o ../mapping/`echo ${i:0:29}`.bam; done ; 
 cd .. 
 
 cd mapping;
-for i in *bam;do samtools fastq -F 3588 `echo ${i:0:29}`.bam | gzip -c > ../human_reads/`echo ${i:0:29}`.fastq.gz;done && 
+for i in *bam;do samtools fastq -F 3588 `echo ${i:0:29}`.bam | gzip -c > ../human_reads/`echo ${i:0:29}`.fastq.gz;done ; 
 cd .. 
 
 cd mapping;
-for i in *bam;do samtools fastq -F 3584 `echo ${i:0:29}`.bam | gzip -c > ../non_human_reads/`echo ${i:0:29}`.fastq.gz;done && 
+for i in *bam;do samtools fastq -F 3584 `echo ${i:0:29}`.bam | gzip -c > ../non_human_reads/`echo ${i:0:29}`.fastq.gz;done ; 
 cd .. 
 
 cd non_human_reads;
-for i in *.fastq.gz; do minimap2 -a -x map-ont -t 10 ../reference_dengue/DENV.reference.fasta `echo ${i:0:29}`.fastq.gz -o ../mapping/`echo ${i:0:29}`_DENV.bam; done && 
+for i in *.fastq.gz; do minimap2 -a -x map-ont -t 10 ../reference_dengue/DENV.reference.fasta `echo ${i:0:29}`.fastq.gz -o ../mapping/`echo ${i:0:29}`_DENV.bam; done ; 
 cd .. 
 
 cd mapping;
-for i in *bam;do samtools fastq -F 3588 `echo ${i:0:29}`_DENV.bam | gzip -c > ../dengue_reads/`echo ${i:0:29}`.fastq.gz;done && 
+for i in *bam;do samtools fastq -F 3588 `echo ${i:0:29}`_DENV.bam | gzip -c > ../dengue_reads/`echo ${i:0:29}`.fastq.gz;done ; 
 cd .. 
 
 cd mapping;
-for i in *bam;do samtools fastq -F 3584 `echo ${i:0:29}`_DENV.bam | gzip -c > ../non_dengue_reads/`echo ${i:0:29}`.fastq.gz;done && 
+for i in *bam;do samtools fastq -F 3584 `echo ${i:0:29}`_DENV.bam | gzip -c > ../non_dengue_reads/`echo ${i:0:29}`.fastq.gz;done ; 
 cd .. 
 
 cd dengue_reads;
-for i in *.fastq.gz; do mkdir -p ../bams/`echo ${i:0:29}` && minimap2 -a -x map-ont -t 10 ../reference_dengue/DENV.reference.fasta $i | samtools view -bS -F 4 - | samtools sort -o ../bams/`echo ${i:0:29}`/`echo ${i:0:29}`.sorted.bam -;done && 
+for i in *.fastq.gz; do mkdir -p ../bams/`echo ${i:0:29}` ; minimap2 -a -x map-ont -t 10 ../reference_dengue/DENV.reference.fasta $i | samtools view -bS -F 4 - | samtools sort -o ../bams/`echo ${i:0:29}`/`echo ${i:0:29}`.sorted.bam -;done ; 
 cd .. 
 
 cd bams;
-for i in *; do samtools index $i/$i.sorted.bam;done && 
+for i in *; do samtools index $i/$i.sorted.bam;done ; 
 cd .. 
 
 cd bams;
-for i in *; do align_trim --normalise 200 ../reference_dengue/DENV.scheme.bed --start --remove-incorrect-pairs --report $i/$i.alignreport.txt < $i/$i.sorted.bam 2> ./$i/$i.alignreport.er | samtools sort -T $i - -o $i/$i.trimmed.rg.sorted.bam; done && 
+for i in *; do align_trim --normalise 200 ../reference_dengue/DENV.scheme.bed --start --remove-incorrect-pairs --report $i/$i.alignreport.txt < $i/$i.sorted.bam 2> ./$i/$i.alignreport.er | samtools sort -T $i - -o $i/$i.trimmed.rg.sorted.bam; done ; 
 cd .. 
 
 cd bams;
-for i in *; do align_trim --normalise 200 ../reference_dengue/DENV.scheme.bed --remove-incorrect-pairs --report $i/$i.alignreport.txt < $i/$i.sorted.bam 2> $i/$i.alignreport.er | samtools sort -T $i - -o $i/$i.primertrimmed.rg.sorted.bam; done && 
+for i in *; do align_trim --normalise 200 ../reference_dengue/DENV.scheme.bed --remove-incorrect-pairs --report $i/$i.alignreport.txt < $i/$i.sorted.bam 2> $i/$i.alignreport.er | samtools sort -T $i - -o $i/$i.primertrimmed.rg.sorted.bam; done ; 
 cd .. 
 
 cd bams;
-for i in *; do samtools index $i/$i.trimmed.rg.sorted.bam | samtools index $i/$i.primertrimmed.rg.sorted.bam; done && 
+for i in *; do samtools index $i/$i.trimmed.rg.sorted.bam | samtools index $i/$i.primertrimmed.rg.sorted.bam; done ; 
 cd .. 
 
 cd bams;
-for i in *; do artic_make_depth_mask --store-rg-depths ../reference_dengue/DENV.reference.fasta $i/$i.primertrimmed.rg.sorted.bam $i/$i.coverage_mask.txt; done && 
+for i in *; do artic_make_depth_mask --store-rg-depths ../reference_dengue/DENV.reference.fasta $i/$i.primertrimmed.rg.sorted.bam $i/$i.coverage_mask.txt; done ; 
 cd .. 
 
 cd bams;
-for i in *; do samtools mpileup -aa -A -Q 0 -d 0 $i/$i.trimmed.rg.sorted.bam | ivar consensus -p $i/$i.fa -m 10 -n N -t 0.5; done && 
+for i in *; do samtools mpileup -aa -A -Q 0 -d 0 $i/$i.trimmed.rg.sorted.bam | ivar consensus -p $i/$i.fa -m 10 -n N -t 0.5; done ; 
 cd .. 
 
 cd bams;
-for i in *barcode*/*.fa ; do cp -r $i ../consensus/; done && 
+for i in *barcode*/*.fa ; do cp -r $i ../consensus/; done ; 
 cd ..
 
 cd consensus;
-bash ../coverage/coverage_fasta.sh; cat *.fa > dengue_run_`echo date`.fasta &&
+python fasta-coverage.py ;
 cd ..
 
 zip results_dengue_run_`echo date` {bams,dengue_reads,human_reads,mapping,non_dengue_reads,non_human_reads,reads,trimmed_reads,visualization_af,visualization_bf} | mv results_dengue_run_* results_dengue;
 mv consensus results_dengue; rm -rf {bams,consensus,dengue_reads,human_reads,mapping,non_dengue_reads,non_human_reads,reads,trimmed_reads,visualization_af,visualization_bf}; rm -rf fastq/*
-
-
-
-
-
--- coverage/coverage_fasta.sh
+++
@@ -1,6 +0,0 @@
-echo -e "File\tSample\tCoverage" > coverage.tsv
-
-for file in *.fa
-do	
-	python /home/artic/data/analysis/20231130_DENGUE_TCHAD/DENGUE/coverage/fasta-coverage.py "$file" >> coverage.tsv
-done
